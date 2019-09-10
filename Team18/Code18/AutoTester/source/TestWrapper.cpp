6f53fdf7086780cfb1d84a98e412f2a5abb7e5d7
LAPTOP-DBVHII6C\Leon <leonchowwenhao@gmail.com>
2019-09-10 14:27:54 +0800

    Edited test queries and source.

--- Team18/Code18/AutoTester/source/TestWrapper.cpp
+++ Team18/Code18/AutoTester/source/TestWrapper.cpp
@@ -41,7 +41,7 @@ void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
   // ...code to evaluate query...
 
 	
-	string evResult = "";
+	//string evResult = "";
 	/*
 	evResult = ev.evalQuery(query);
 	*/
@@ -49,6 +49,6 @@ void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
   // store the answers to the query in the results list (it is initially empty)
   // each result must be a string.
 
-	results.push_front(evResult);	
+	//results.push_front(evResult);	
 
 }
