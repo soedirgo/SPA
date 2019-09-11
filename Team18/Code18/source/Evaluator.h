08163e09fb335ba2f2a08d4ba54c6577d2916ada
dan-delion <sram-b@comp.nus.edu.sg>
2019-09-11 18:28:41 +0800

    add evaluator and evaluator tests

--- Team18/Code18/source/Evaluator.h
+++ Team18/Code18/source/Evaluator.h
@@ -4,14 +4,6 @@
 #include <utility>
 #include "Query.h"
 
-// class Evaluator {
-// public:
-//     static std::string evalQuery(Query q);
-// private:
-//     static std::vector<std::string, std::string> declarations;
-//     static std::vector<Clause> clauses;
-// };
-
 namespace Evaluator {
-    std::string evalQuery(Query q);
+	std::string evalQuery(Query q);
 }
