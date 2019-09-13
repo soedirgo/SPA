0c7749ef2bf33bbfc33f7d500ad6c3a192cb76d8
Hansel Ng <hanselblack@hotmail.com>
2019-09-13 23:02:59 +0800

    Fixed TestQueryParser

---
+++ Team18/Code18/UnitTesting/TestProgramParser.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Parser.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(TestProgramParser)
+	{
+	public:
+
+		TEST_METHOD(ProcTest)
+		{
+			Parser parser = Parser();
+			string input = "procedure main {";
+			string expected = "main";
+			string actual = parser.parseProc(input);
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(ReadTest)
+		{
+			Parser parser = Parser();
+			string input = "read x;";
+			string expected = "x";
+			string actual = parser.parseRead(input);
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(PrintTest)
+		{
+			Parser parser = Parser();
+			string input = "print x;";
+			string expected = "x";
+			string actual = parser.parsePrint(input);
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(AssignTest)
+		{
+			Parser parser = Parser();
+			string input = "x = a;";
+			string expected = "x=a";
+			string actual = parser.parseAssignInit(input);
+			Assert::AreEqual(expected, actual);
+		}
+
+	};
+}
