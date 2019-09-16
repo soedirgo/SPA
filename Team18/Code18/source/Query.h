48e3424af54dd25ab41dca972676cd907b70e9f1
unknown <jingzhe321@gmail.com>
2019-09-16 17:44:19 +0800

    splitPattern, Unittesting and refactoring

--- Team18/Code18/source/Query.h
+++ Team18/Code18/source/Query.h
@@ -9,12 +9,15 @@ class Query {
 public:
 	Query(std::unordered_map<std::string, std::string> declarations,
 		std::string selectSynonym,
-		std::vector<std::pair<std::string, std::pair<std::string, std::string>>> clauses);
+		std::vector<std::pair<std::string, std::pair<std::string, std::string>>> clauses,
+		std::vector<std::pair<std::string, std::pair<std::string, std::string>>> patternclauses);
 	std::unordered_map<std::string, std::string> getDeclarations();
 	std::string getSelectSynonym();
 	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> getClauses();
+	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> getPatternClauses();
 private:
 	std::unordered_map<std::string, std::string> declarations;
 	std::string selectSynonym;
 	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> clauses;
+	std::vector<std::pair<std::string, std::pair<std::string, std::string>>> patternclauses;
 };
--- Team18/Code18/source/QueryParser.cpp
+++ Team18/Code18/source/QueryParser.cpp
@@ -21,42 +21,72 @@ list<string> QueryParser::parse(string query) {
 
 	string selectVars;
 	vector<pair<string, pair<string, string>>> suchThat;
-	vector<string> suchThatClauses;
+	vector<pair<string, pair<string, string>>> pattern;
 
-	//Finds the first occurance of such that in the string
+	vector<string> suchThatClauses;
+	vector<string> patternClauses;
+
+	//Finds the first occurance of such that and pattern in the string
 	int suchThatIndex = select.find("such that ");
+	int patternIndex = select.find("pattern ");
+	int currentIndex;
+
+	if (suchThatIndex < patternIndex) {
+		currentIndex = suchThatIndex;
+	}
+
+	else {
+		currentIndex = patternIndex;
+	}
 
 	//Only have select variables
-	if (suchThatIndex == -1) {
+	if (currentIndex == -1) {
 		selectVars = splitSelect(select);
 	}
 
 	//Split the word select out and remove it away from the select string
 	else {
-		selectVars = splitSelect(select.substr(0, suchThatIndex));
-		select = select.substr(suchThatIndex);
+		selectVars = splitSelect(select.substr(0, currentIndex));
+		select = select.substr(currentIndex);
 	}
 	
-	//Find the next occurance of such that, push it to SuchThatClauses. Continues till no more select statements
-	while (select.length() > 0) {
+	//Find the next occurance of such that, push it to SuchThatClauses/PatternClauses. Continues till no more select statements
+	while (select.length() > 0 && currentIndex != -1) {
 		suchThatIndex = select.substr(1).find(" such that");
-		if (suchThatIndex == -1) {
-			break;
+		patternIndex = select.substr(1).find(" pattern ");
+		if (suchThatIndex < patternIndex) {
+			currentIndex = suchThatIndex;
 		}
+
+		else {
+			currentIndex = patternIndex;
+		}
+		if (currentIndex == 0) {
+			currentIndex = select.length();
+		}
+
+		else {
+			currentIndex = currentIndex + 1;
+		}
+
 		string currentClause = select.substr(0, suchThatIndex);
 		if (currentClause.find("such that ")) {
 			suchThatClauses.push_back(currentClause);
+		}
+
+		else if (currentClause.find("pattern ")) {
+			patternClauses.push_back(currentClause);
 		}
 		
 		select = select.substr(suchThatIndex);
 	}
 	
 	suchThat = splitSuchThat(suchThatClauses);
+	pattern = splitPattern(patternClauses);
 	
-	Query q = Query(declerationVariables, selectVars, suchThat);
+	Query q = Query(declerationVariables, selectVars, suchThat, pattern);
 	list<string> finalOutput = evalQuery(q);
 	
-	//return "";
 	return finalOutput;
 	
 }
@@ -179,6 +209,36 @@ vector<pair<string, pair<string, string>>> QueryParser::splitSuchThat(vector<str
 		else {
 			secondVar = removeSpaces(suchThat[i].substr(posOfComma + 1, posOfCloseBracket - posOfComma - 1), whitespace);
 		}
+
+		s.push_back(make_pair(clauseType, make_pair(firstVar, secondVar)));
+	}
+
+	return s;
+}
+
+//Split by brackets
+vector<pair<string, pair<string, string>>> QueryParser::splitPattern(vector<string> pattern) {
+	vector<pair<string, pair<string, string>>> s;
+	int i = 0;
+	int posOfOpenBracket;
+	int posOfComma;
+	int posOfCloseBracket;
+	int lengthofString;
+	string clauseType;
+	for (i = 0; i < pattern.size(); i++) {
+		posOfOpenBracket = pattern[i].find_first_of("(");
+		posOfCloseBracket = pattern[i].find_last_of(")");
+		posOfComma = pattern[i].find(",");
+
+		if (pattern[i].find("pattern") != -1) {
+			clauseType = trim(pattern[i].substr(7, posOfOpenBracket - 7), whitespace);
+		}
+		else {
+			clauseType = trim(pattern[i].substr(3, posOfOpenBracket - 3), whitespace);
+		}
+
+		string firstVar = trim(pattern[i].substr(posOfOpenBracket + 1, posOfComma - posOfOpenBracket - 1),whitespace);
+		string secondVar = removeSpaces(pattern[i].substr(posOfComma + 1, posOfCloseBracket - posOfComma - 1),whitespace);
 
 		s.push_back(make_pair(clauseType, make_pair(firstVar, secondVar)));
 	}
