dff7503f120fbb5e2d9e049889619606c2cc3f25
Bobbie R. Soedirgo <sram-b@comp.nus.edu.sg>
2019-09-18 20:09:52 +0800

    add follows and parent evaluator tests

--- Team18/Code18/IntegrationTesting/TestEvaluator.cpp
+++ Team18/Code18/IntegrationTesting/TestEvaluator.cpp
@@ -36,6 +36,7 @@ namespace UnitTesting
             pkb.clear();
 
             pkb.setProc("main");
+            
             pkb.setStmt(1, Assign);
             pkb.setAssignStmt(1, "x");
             pkb.setVar("x");
@@ -47,35 +48,50 @@ namespace UnitTesting
             pkb.setVar("y");
             pkb.setUsesVarByStmt(2, "x");
             pkb.setModifiesVarByStmt(2, "y");
+            pkb.setFollow(1, 2);
+            pkb.setFollowStar(1, 2);
 
             pkb.setStmt(3, If);
             pkb.setIfStmt(3);
             pkb.setUsesVarByStmt(3, "x");
             pkb.setUsesVarByStmt(3, "y");
             pkb.setModifiesVarByStmt(3, "z");
+            pkb.setFollow(2, 3);
+            pkb.setFollowStar(1, 3);
+            pkb.setFollowStar(2, 3);
 
             pkb.setStmt(4, Assign);
             pkb.setAssignStmt(4, "z");
             pkb.setVar("z");
             pkb.setConstant("0");
             pkb.setModifiesVarByStmt(4, "z");
+            pkb.setParent(3, 4);
 
             pkb.setStmt(5, Assign);
             pkb.setAssignStmt(5, "z");
             pkb.setModifiesVarByStmt(5, "z");
+            pkb.setParent(4, 5);
 
             pkb.setStmt(6, While);
             pkb.setWhileStmt(6);
             pkb.setUsesVarByStmt(6, "x");
             pkb.setModifiesVarByStmt(6, "x");
+            pkb.setFollow(3, 6);
+            pkb.setFollowStar(1, 6);
+            pkb.setFollowStar(2, 6);
+            pkb.setFollowStar(3, 6);
 
             pkb.setStmt(7, Read);
             pkb.setReadStmt(7, "x");
             pkb.setModifiesVarByStmt(7, "x");
+            pkb.setParent(6, 7);
 
             pkb.setStmt(8, Print);
             pkb.setPrintStmt(8, "x");
             pkb.setUsesVarByStmt(8, "x");
+            pkb.setParent(6, 8);
+            pkb.setFollow(7, 8);
+            pkb.setFollowStar(7, 8);
 		}
         TEST_METHOD(evaluatorNoClause)
         {
@@ -127,7 +143,7 @@ namespace UnitTesting
             actual = Evaluator::evalQuery(Query(decl, "P", {}, {}));
             Assert::IsTrue(expected == actual);
         }
-        TEST_METHOD(evaluatorSuchThat)
+        TEST_METHOD(evaluatorUsesModifies)
         {
             /**
                To test:
@@ -154,46 +170,107 @@ namespace UnitTesting
             list<string> actual;
 
             expected = {"2", "3", "6", "8"};
-			actual = Evaluator::evalQuery(Query(decl, "s", { {"uses", {"s", "v"}} }, {}));
+			actual = Evaluator::evalQuery(Query(decl, "s", { {"Uses", {"s", "v"}} }, {}));
             Assert::IsTrue(expected == actual);
  
             expected = {"7"};
-			actual = Evaluator::evalQuery(Query(decl, "r", { {"modifies", {"r", "v"}} }, {}));
+			actual = Evaluator::evalQuery(Query(decl, "r", { {"Modifies", {"r", "v"}} }, {}));
             Assert::IsTrue(expected == actual);
 
             expected = {"8"};
-			actual = Evaluator::evalQuery(Query(decl, "p", { {"uses", {"p", "v"}} }, {}));
+			actual = Evaluator::evalQuery(Query(decl, "p", { {"Uses", {"p", "v"}} }, {}));
             Assert::IsTrue(expected == actual);
 
             expected = {"6"};
-            actual = Evaluator::evalQuery(Query(decl, "w", { {"uses", {"w", "v"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "w", { {"Uses", {"w", "v"}} }, {}));
             Assert::IsTrue(expected == actual);
 
             expected = {};
-            actual = Evaluator::evalQuery(Query(decl, "i", { {"uses", {"i", "z"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "i", { {"Uses", {"i", "z"}} }, {}));
             Assert::IsTrue(expected == actual);
  
             expected = {"1"};
-            actual = Evaluator::evalQuery(Query(decl, "a", { {"modifies", {"a", "x"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "a", { {"Modifies", {"a", "x"}} }, {}));
             Assert::IsTrue(expected == actual);
 
             expected = {"x"};
-            actual = Evaluator::evalQuery(Query(decl, "v", { {"uses", {"2", "v"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "v", { {"Uses", {"2", "v"}} }, {}));
             Assert::IsTrue(expected == actual);
 
             expected = {"1", "0"};
-            actual = Evaluator::evalQuery(Query(decl, "C", { {"uses", {"2", "x"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "C", { {"Uses", {"2", "x"}} }, {}));
             Assert::IsTrue(expected == actual);
 
             //expected = {"main"};
-            //actual = Evaluator::evalQuery(Query(decl, "P", { {"modifies", {"P", "z"}} }));
+            //actual = Evaluator::evalQuery(Query(decl, "P", { {"Modifies", {"P", "z"}} }));
             //Assert::IsTrue(expected == actual);
         }
-        TEST_METHOD(evaluatorPattern)
+        TEST_METHOD(evaluatorFollowsParent)
         {
-        }
-        TEST_METHOD(evaluatorSuchThatPattern)
-        {
+            /**
+               1. Select s such that Follows(1, s);
+               2. Select s such that Follows(s, 6);
+               3. Select s such that Follows*(1, s);
+               4. Select r such that Follows(r, p);
+               5. Select a such that Follows*(a, w);
+               6. Select s such that Parent(3, s);
+               7. Select a such that Parent(i, a);
+               8. Select s such that Parent(4, s);
+               9. Select p such that Parent*(w, p);
+               10.Select a such that Parent*(s, a);
+             */
+            unordered_map<string, string> decl = { {"s", "stmt"},
+                                                   {"r", "read"},
+                                                   {"p", "print"},
+                                                   {"w", "while"},
+                                                   {"i", "if"},
+                                                   {"a", "assign"},
+                                                   {"v", "variable"},
+                                                   {"C", "constant"},
+                                                   {"P", "procedure"} };
+            list<string> expected;
+            list<string> actual;
+            
+            expected = {"2"};
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Follows", {"1", "s"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
+            expected = {"3"};
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Follows", {"s", "6"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
+            expected = {"2", "3", "6"};
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Follows*", {"1", "s"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
+            expected = {"7"};
+            actual = Evaluator::evalQuery(Query(decl, "r", { {"Follows", {"r", "p"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
+            expected = {"1", "2"};
+            actual = Evaluator::evalQuery(Query(decl, "a", { {"Follows*", {"a", "w"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
+            expected = {"4", "5"};
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Parent", {"3", "s"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
+            expected = {"4", "5"};
+            actual = Evaluator::evalQuery(Query(decl, "a", { {"Parent", {"i", "a"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
+            expected = {};
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Parent", {"4", "s"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
+            expected = {"8"};
+            actual = Evaluator::evalQuery(Query(decl, "p", { {"Parent*", {"w", "p"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
+            expected = {"4", "5"};
+            actual = Evaluator::evalQuery(Query(decl, "a", { {"Parent*", {"s", "a"}} }, {}));
+            Assert::IsTrue(expected == actual);
+
         }
 	};
 }
