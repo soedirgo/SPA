befd1e7c5f8e1fb467063e20b0fe36d65197d6d8
dan-delion <sram-b@comp.nus.edu.sg>
2019-09-18 20:43:22 +0800

    fix evaluator tests for follows and parent

--- Team18/Code18/IntegrationTesting/TestEvaluator.cpp
+++ Team18/Code18/IntegrationTesting/TestEvaluator.cpp
@@ -4,11 +4,13 @@
 #include "Clause.h"
 #include "Evaluator.h"
 #include "PKB.h"
+#include "PKBFollow.h"
+#include "PKBParent.h"
 
 using namespace std;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTesting
+namespace IntegrationTesting
 {
 	TEST_CLASS(TestEvaluator)
 	{
@@ -34,6 +36,8 @@ namespace UnitTesting
             PKB pkb;
 
             pkb.clear();
+            pkb.clear();
+            pkb.clear();
 
             pkb.setProc("main");
             
@@ -48,50 +52,54 @@ namespace UnitTesting
             pkb.setVar("y");
             pkb.setUsesVarByStmt(2, "x");
             pkb.setModifiesVarByStmt(2, "y");
-            pkb.setFollow(1, 2);
-            pkb.setFollowStar(1, 2);
+            pkb.insertFollowRelation(1, 2);
+            pkb.insertFollowStarRelation(1, 2);
 
             pkb.setStmt(3, If);
             pkb.setIfStmt(3);
             pkb.setUsesVarByStmt(3, "x");
             pkb.setUsesVarByStmt(3, "y");
             pkb.setModifiesVarByStmt(3, "z");
-            pkb.setFollow(2, 3);
-            pkb.setFollowStar(1, 3);
-            pkb.setFollowStar(2, 3);
+            pkb.insertFollowRelation(2, 3);
+            pkb.insertFollowStarRelation(1, 3);
+            pkb.insertFollowStarRelation(2, 3);
 
             pkb.setStmt(4, Assign);
             pkb.setAssignStmt(4, "z");
             pkb.setVar("z");
             pkb.setConstant("0");
             pkb.setModifiesVarByStmt(4, "z");
-            pkb.setParent(3, 4);
+            pkb.insertParentRelation(3, 4);
+            pkb.insertParentStarRelation(3, 4);
 
             pkb.setStmt(5, Assign);
             pkb.setAssignStmt(5, "z");
             pkb.setModifiesVarByStmt(5, "z");
-            pkb.setParent(4, 5);
+            pkb.insertParentRelation(3, 5);
+            pkb.insertParentStarRelation(3, 5);
 
             pkb.setStmt(6, While);
             pkb.setWhileStmt(6);
             pkb.setUsesVarByStmt(6, "x");
             pkb.setModifiesVarByStmt(6, "x");
-            pkb.setFollow(3, 6);
-            pkb.setFollowStar(1, 6);
-            pkb.setFollowStar(2, 6);
-            pkb.setFollowStar(3, 6);
+            pkb.insertFollowRelation(3, 6);
+            pkb.insertFollowStarRelation(1, 6);
+            pkb.insertFollowStarRelation(2, 6);
+            pkb.insertFollowStarRelation(3, 6);
 
             pkb.setStmt(7, Read);
             pkb.setReadStmt(7, "x");
             pkb.setModifiesVarByStmt(7, "x");
-            pkb.setParent(6, 7);
+            pkb.insertParentRelation(6, 7);
+            pkb.insertParentStarRelation(6, 7);
 
             pkb.setStmt(8, Print);
             pkb.setPrintStmt(8, "x");
             pkb.setUsesVarByStmt(8, "x");
-            pkb.setParent(6, 8);
-            pkb.setFollow(7, 8);
-            pkb.setFollowStar(7, 8);
+            pkb.insertParentRelation(6, 8);
+            pkb.insertParentStarRelation(6, 8);
+            pkb.insertFollowRelation(7, 8);
+            pkb.insertFollowStarRelation(7, 8);
 		}
         TEST_METHOD(evaluatorNoClause)
         {
