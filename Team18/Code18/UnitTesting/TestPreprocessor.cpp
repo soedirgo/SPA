01d35b58c23bd52dd00302117df4d86c2fdb19be
Bobbie R. Soedirgo <sram-b@comp.nus.edu.sg>
2019-09-24 10:32:14 +0700

    add preprocessor unit tests

--- Team18/Code18/UnitTesting/TestPreprocessor.cpp
+++ Team18/Code18/UnitTesting/TestPreprocessor.cpp
@@ -38,88 +38,66 @@ namespace UnitTesting
         }
         TEST_METHOD(invalidQueries)
         {
-            //list<string> expected;
-            //list<string> actual;
+            string input;
 
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("foo");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("Select");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("Select 1");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("Select s");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s; Selct s");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s; select s");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s Select s");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("foo s; Select s");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s 1; Select s");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s*; Select s*");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s, Select s");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("assign a; Select a such that Uses(a)");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s; Select s suchthat Uses(s, \"x\")");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s; variable v; Select s Uses(s, v)");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s1, s2; Select s such that Foo(s1, s2)");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s; variable v; Select s such that Uses(s, v) pattern (v, _)");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("assign a; variable v; Select a such that Uses(a, v) and pattern a(v, _)");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s; variable v; Select s such that Uses(_, v)");
-            //Assert::IsTrue(expected == actual);
-
-            //expected = { "Invalid" };
-            //actual = QueryParser::parse("stmt s; variable v; Select s such that Uses(v, s)");
-            //Assert::IsTrue(expected == actual);
-        }
+            input = "";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "foo";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "Select";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "Select 1";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "Select s";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s; Selct s";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s; select s";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s Select s";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "foo s; Select s";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s 1; Select s";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s*; Select s*";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s, Select s";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "assign a; Select a such that Uses(a)";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s; Select s suchthat Uses(s, \"x\")";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s; variable v; Select s Uses(s, v)";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s1, s2; Select s such that Foo(s1, s2)";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s; variable v; Select s such that Uses(s, v) pattern (v, _)";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "assign a; variable v; Select a such that Uses(a, v) and pattern a(v, _)";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s; variable v; Select s such that Uses(_, v)";
+            Assert::IsFalse(Preprocessor::isValid(input));
+            
+            input = "stmt s; variable v; Select s such that Uses(v, s)";
+            Assert::IsFalse(Preprocessor::isValid(input));
     };
 }
