e297ead6bc1275664ddc4c092f09e24a8111a7f5
Hansel Ng <hanselblack@hotmail.com>
2019-09-24 19:01:40 +0800

    Refactor design extractor recursion

--- Team18/Code18/source/DesignExtractor.h
+++ Team18/Code18/source/DesignExtractor.h
@@ -11,6 +11,6 @@ public:
 private:
 	static void extractFollowStar();
 	static void extractParentStar();
-	static void recurseParent(int parent, std::unordered_set<int> tempChildList);
+	static void recurseParent(int parent, int tempChildList);
 	static void recurseFollow(int followedBy, int follow);
 };
