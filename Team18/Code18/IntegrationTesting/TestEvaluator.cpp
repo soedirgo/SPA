03b0e6bc0dc247717f39bae8b3c10350cf3ad797
dan-delion <sram-b@comp.nus.edu.sg>
2019-10-13 18:37:56 +0800

    fix TestEvaluator partially

--- Team18/Code18/IntegrationTesting/TestEvaluator.cpp
+++ Team18/Code18/IntegrationTesting/TestEvaluator.cpp
@@ -10,7 +10,6 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace IntegrationTesting
 {
-	/*
 	TEST_CLASS(TestEvaluator)
 	{
 	public:
@@ -32,76 +31,73 @@ namespace IntegrationTesting
                8.          print x;
                _       }
             */
-			/*
             PKB pkb;
 
             pkb.clear();
 
             pkb.setProcedure("main");
             
-            pkb.setStmt(1, Assign);
-            pkb.setAssignStmt(1, "x");
+            pkb.setStmt("1", "assign");
+            pkb.setAssignPattern("1", "1");
             pkb.setVariable("x");
-            pkb.setConstant("1", 1);
-            pkb.setModifiesS(1, "x");
+            pkb.setConstant("1", "1");
+            pkb.setModifiesS("1", "x");
 
-            pkb.setStmt(2, Assign);
-            pkb.setAssignStmt(2, "y");
+            pkb.setStmt("2", "assign");
+            pkb.setAssignPattern("2", "x");
             pkb.setVariable("y");
-            pkb.setUsesS(2, "x");
-            pkb.setModifiesS(2, "y");
-            pkb.setFollows(1, 2);
-            pkb.setFollowsT(1, 2);
+            pkb.setUsesS("2", "x");
+            pkb.setModifiesS("2", "y");
+            pkb.setFollows("1", "2");
+            pkb.setFollowsT("1", "2");
 
-            pkb.setStmt(3, If);
-            //pkb.setIfStmt(3);
-            pkb.setUsesS(3, "x");
-            pkb.setUsesS(3, "y");
-            pkb.setModifiesS(3, "z");
-            pkb.setFollows(2, 3);
-            pkb.setFollowsT(1, 3);
-            pkb.setFollowsT(2, 3);
+            pkb.setStmt("3", "if");
+            //pkb.setIfPattern("3", "(x y ==)");
+            pkb.setUsesS("3", "x");
+            pkb.setUsesS("3", "y");
+            pkb.setModifiesS("3", "z");
+            pkb.setFollows("2", "3");
+            pkb.setFollowsT("1", "3");
+            pkb.setFollowsT("2", "3");
 
-            pkb.setStmt(4, Assign);
-            pkb.setAssignStmt(4, "z");
+            pkb.setStmt("4", "assign");
+            pkb.setAssignPattern("4", "0");
             pkb.setVariable("z");
-            pkb.setConstant("0", 4);
-            pkb.setModifiesS(4, "z");
-            pkb.setParent(3, 4);
-            pkb.setParentT(3, 4);
+            pkb.setConstant("4", "0");
+            pkb.setModifiesS("4", "z");
+            pkb.setParent("3", "4");
+            pkb.setParentT("3", "4");
 
-            pkb.setStmt(5, Assign);
-            pkb.setAssignStmt(5, "z");
-            pkb.setConstant("1", 5);
-            pkb.setModifiesS(5, "z");
-            pkb.setParent(3, 5);
-            pkb.setParentT(3, 5);
+            pkb.setStmt("5", "assign");
+            pkb.setAssignPattern("5", "1");
+            pkb.setConstant("5", "1");
+            pkb.setModifiesS("5", "z");
+            pkb.setParent("3", "5");
+            pkb.setParentT("3", "5");
 
-            pkb.setStmt(6, While);
-            //pkb.setWhileStmt(6);
-            pkb.setUsesS(6, "x");
-            pkb.setModifiesS(6, "x");
-            pkb.setFollows(3, 6);
-            pkb.setFollowsT(1, 6);
-            pkb.setFollowsT(2, 6);
-            pkb.setFollowsT(3, 6);
+            pkb.setStmt("6", "while");
+            //pkb.setWhilePattern("6", "(x 0 >)");
+            pkb.setUsesS("6", "x");
+            pkb.setModifiesS("6", "x");
+            pkb.setFollows("3", "6");
+            pkb.setFollowsT("1", "6");
+            pkb.setFollowsT("2", "6");
+            pkb.setFollowsT("3", "6");
 
-            pkb.setStmt(7, Read);
-            pkb.setRead(7, "x");
-            pkb.setModifiesS(7, "x");
-            pkb.setParent(6, 7);
-            pkb.setParentT(6, 7);
+            pkb.setStmt("7", "read");
+            pkb.setRead("7", "x");
+            pkb.setModifiesS("7", "x");
+            pkb.setParent("6", "7");
+            pkb.setParentT("6", "7");
 
-            pkb.setStmt(8, Print);
-            pkb.setPrint(8, "x");
-            pkb.setUsesS(8, "x");
-            pkb.setParent(6, 8);
-            pkb.setParentT(6, 8);
-            pkb.setFollows(7, 8);
-            pkb.setFollowsT(7, 8);
-			*/
+            pkb.setStmt("8", "print");
+            pkb.setPrint("8", "x");
+            pkb.setUsesS("8", "x");
+            pkb.setParent("6", "8");
+            pkb.setParentT("6", "8");
+            pkb.setFollows("7", "8");
+            pkb.setFollowsT("7", "8");
 		}
-		/*
         TEST_METHOD(evaluatorNoClause)
         {
             unordered_map<string, string> decl = { {"s", "stmt"},
@@ -117,39 +113,39 @@ namespace IntegrationTesting
             list<string> actual;
 
             expected = { "1", "2", "3", "4", "5", "6", "7", "8" };
-			actual = Evaluator::evalQuery(Query(decl, "s", {}, {}));
+			actual = Evaluator::evalQuery(Query(decl, "s", {}));
             Assert::IsTrue(expected == actual);
 
             expected = { "7" };
-            actual = Evaluator::evalQuery(Query(decl, "r", {}, {}));
+            actual = Evaluator::evalQuery(Query(decl, "r", {}));
             Assert::IsTrue(expected == actual);
 
             expected = { "8" };
-            actual = Evaluator::evalQuery(Query(decl, "p", {}, {}));
+            actual = Evaluator::evalQuery(Query(decl, "p", {}));
             Assert::IsTrue(expected == actual);
 
             expected = { "6" };
-            actual = Evaluator::evalQuery(Query(decl, "w", {}, {}));
+            actual = Evaluator::evalQuery(Query(decl, "w", {}));
             Assert::IsTrue(expected == actual);
 
             expected = { "3" };
-            actual = Evaluator::evalQuery(Query(decl, "i", {}, {}));
+            actual = Evaluator::evalQuery(Query(decl, "i", {}));
             Assert::IsTrue(expected == actual);
 
             expected = { "1", "2", "4", "5" };
-            actual = Evaluator::evalQuery(Query(decl, "a", {}, {}));
+            actual = Evaluator::evalQuery(Query(decl, "a", {}));
             Assert::IsTrue(expected == actual);
 
             expected = { "x", "y", "z" };
-            actual = Evaluator::evalQuery(Query(decl, "v", {}, {}));
+            actual = Evaluator::evalQuery(Query(decl, "v", {}));
             Assert::IsTrue(expected == actual);
 
             expected = { "1", "0" };
-            actual = Evaluator::evalQuery(Query(decl, "C", {}, {}));
+            actual = Evaluator::evalQuery(Query(decl, "C", {}));
             Assert::IsTrue(expected == actual);
 
             expected = { "main" };
-            actual = Evaluator::evalQuery(Query(decl, "P", {}, {}));
+            actual = Evaluator::evalQuery(Query(decl, "P", {}));
             Assert::IsTrue(expected == actual);
         }
         TEST_METHOD(evaluatorUsesModifies)
@@ -166,7 +162,6 @@ namespace IntegrationTesting
                8. Select C such that Uses(2, "x");
                9. Select P such that Modifies(P, "z");
             */
-		/*
             unordered_map<string, string> decl = { {"s", "stmt"},
                                                    {"r", "read"},
                                                    {"p", "print"},
@@ -180,35 +175,35 @@ namespace IntegrationTesting
             list<string> actual;
 
             expected = {"2", "3", "6", "8"};
-			actual = Evaluator::evalQuery(Query(decl, "s", { {"Uses", {"s", "v"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "s", { Clause("such-that", {"Uses", "s", "v"}) }));
             Assert::IsTrue(expected == actual);
  
             expected = {"7"};
-			actual = Evaluator::evalQuery(Query(decl, "r", { {"Modifies", {"r", "v"}} }, {}));
+			actual = Evaluator::evalQuery(Query(decl, "r", { {"Modifies", {"r", "v"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"8"};
-			actual = Evaluator::evalQuery(Query(decl, "p", { {"Uses", {"p", "v"}} }, {}));
+			actual = Evaluator::evalQuery(Query(decl, "p", { {"Uses", {"p", "v"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"6"};
-            actual = Evaluator::evalQuery(Query(decl, "w", { {"Uses", {"w", "v"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "w", { {"Uses", {"w", "v"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {};
-            actual = Evaluator::evalQuery(Query(decl, "i", { {"Uses", {"i", "\"z\""}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "i", { {"Uses", {"i", "\"z\""}} }));
             Assert::IsTrue(expected == actual);
  
             expected = {"1"};
-            actual = Evaluator::evalQuery(Query(decl, "a", { {"Modifies", {"a", "\"x\""}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "a", { {"Modifies", {"a", "\"x\""}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"x"};
-            actual = Evaluator::evalQuery(Query(decl, "v", { {"Uses", {"2", "v"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "v", { {"Uses", {"2", "v"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"1", "0"};
-            actual = Evaluator::evalQuery(Query(decl, "C", { {"Uses", {"2", "\"x\""}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "C", { {"Uses", {"2", "\"x\""}} }));
             Assert::IsTrue(expected == actual);
 
             //expected = {"main"};
@@ -229,7 +224,6 @@ namespace IntegrationTesting
                9. Select p such that Parent*(w, p);
                10.Select a such that Parent*(s, a);
              */
-			 /**
             unordered_map<string, string> decl = { {"s", "stmt"},
                                                    {"r", "read"},
                                                    {"p", "print"},
@@ -243,43 +237,43 @@ namespace IntegrationTesting
             list<string> actual;
             
             expected = {"2"};
-            actual = Evaluator::evalQuery(Query(decl, "s", { {"Follows", {"1", "s"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Follows", {"1", "s"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"3"};
-            actual = Evaluator::evalQuery(Query(decl, "s", { {"Follows", {"s", "6"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Follows", {"s", "6"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"2", "3", "6"};
-            actual = Evaluator::evalQuery(Query(decl, "s", { {"Follows*", {"1", "s"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Follows*", {"1", "s"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"7"};
-            actual = Evaluator::evalQuery(Query(decl, "r", { {"Follows", {"r", "p"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "r", { {"Follows", {"r", "p"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"1", "2"};
-            actual = Evaluator::evalQuery(Query(decl, "a", { {"Follows*", {"a", "w"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "a", { {"Follows*", {"a", "w"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"4", "5"};
-            actual = Evaluator::evalQuery(Query(decl, "s", { {"Parent", {"3", "s"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Parent", {"3", "s"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"4", "5"};
-            actual = Evaluator::evalQuery(Query(decl, "a", { {"Parent", {"i", "a"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "a", { {"Parent", {"i", "a"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {};
-            actual = Evaluator::evalQuery(Query(decl, "s", { {"Parent", {"4", "s"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "s", { {"Parent", {"4", "s"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"8"};
-            actual = Evaluator::evalQuery(Query(decl, "p", { {"Parent*", {"w", "p"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "p", { {"Parent*", {"w", "p"}} }));
             Assert::IsTrue(expected == actual);
 
             expected = {"4", "5"};
-            actual = Evaluator::evalQuery(Query(decl, "a", { {"Parent*", {"s", "a"}} }, {}));
+            actual = Evaluator::evalQuery(Query(decl, "a", { {"Parent*", {"s", "a"}} }));
             Assert::IsTrue(expected == actual);
         }
         TEST_METHOD(evaluatorPattern)
@@ -290,7 +284,6 @@ namespace IntegrationTesting
                3. Select a pattern a(_, 1)
                4. Select a pattern a(z, 0)
              */
-			 /**
             unordered_map<string, string> decl = { {"s", "stmt"},
                                                    {"r", "read"},
                                                    {"p", "print"},
@@ -323,4 +316,3 @@ namespace IntegrationTesting
 	};
 
 }
-	*/
