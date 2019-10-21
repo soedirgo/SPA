0f6ff0c77c1245fad82d39f1322eb04371fcffb1
Bobbie Soedirgo <sram-b@comp.nus.edu.sg>
2019-10-22 01:11:37 +0800

    add pattern validation

--- Team18/Code18/source/Validator.h
+++ Team18/Code18/source/Validator.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace Validator {
-    bool isValid(std::string& input);
+    bool isPatternValid(std::vector<std::string>& fields,
+                        std::unordered_map<std::string, std::string>& declarationMap);
 }
